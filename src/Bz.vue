d4bd5d2f64a9025a3adba2d21dbe46ce4feb2283
bigzhu <vermiliondun@gmail.com>
2017-03-15 14:31:19 +0800

    可以用

---
+++ src/Bz.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div v-for="d in datas" class="swiper-slide" >
+        <a :href="d.url"><img class="img slide" :src="d.src"  alt=""></a>
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+  import 'swiper/dist/css/swiper.min.css'
+  import Swiper from 'swiper'
+
+  export default {
+    props: {
+      datas: {
+        type: Array,
+        default: []
+      }
+    },
+    components: {
+    },
+    computed: {
+    },
+    data: function () {
+      return {
+      }
+    },
+    mounted: function () {
+      this.$nextTick(function () {
+        this.initSwiper()
+      })
+    },
+    methods: {
+      initSwiper: function () {
+        let conf = {
+          speed: 1400,
+          spaceBetween: 100,
+          autoplay: 3000,
+          loop: true,
+          pagination: '.swiper-pagination'
+        }
+        setTimeout(function () {
+          let a = Swiper('.swiper-container', conf)
+          console.log(a)
+        }, 3)
+      }
+    }
+  }
+</script>
+<style>
+  .img.slide {
+    width: 100%;
+  }
+  .white-bz {
+    background-color: white;
+  }
+  .swiper-pagination {
+    top: 85%;
+    .swiper-pagination-bullet{ opacity: 1; }
+    .swiper-pagination-bullet-active { background: white; opacity: 1; }
+  }
+</style>
---
+++ src/Doc.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <doc :name="name"
+      :desc="desc"
+      :parm_desc="parm_desc"
+      :parms="parms"
+      :code="code"
+      >
+      <bz :datas="datas"></bz>
+    </doc>
+  </div>
+</template>
+<script>
+  import Bz from './Bz'
+  import Doc from 'bz-doc'
+  export default {
+    components: {
+      Bz,
+      Doc
+    },
+    data: function () {
+      return {
+        name: 'bz-swiper',
+        desc: '登录页面',
+        datas: [
+          {url: '/bigzhu', src: 'https://m.gotosiam.com/static/img/home-slide-01.8e9068a.jpg'},
+          {url: '/bigzhu', src: 'https://m.gotosiam.com/static/img/home-slide-02.f47b925.jpg'}
+        ],
+        parms: [
+          {parm: 'datas', desc: '一个list, ojbect 有 url 和 src 两个字段'}
+        ],
+        parm_desc: ``,
+        code: `<bz :datas="datas"></bz>`
+      }
+    },
+    methods: {
+    }
+  }
+</script>
